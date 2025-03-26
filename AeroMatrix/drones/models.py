26951b87cd1108fd5a72702cf07c569a808888ab
Adrián Jiménez Santiago <ajimsan2009@g.educaand.es>
2025-03-26 13:37:30 +0100

    clase enum creada con django

--- AeroMatrix/drones/models.py
+++ AeroMatrix/drones/models.py
@@ -1,3 +1,31 @@
-from django.db import models
 
-# Create your models here.
+from django.db import models
+import enum
+
+class OrientationEnum(enum.Enum):
+    N = "N"
+    S = "S"
+    E = "E"
+    O = "O"
+
+    # Métodos para girar 
+    def turn_left(self):
+        mapping = {
+            OrientationEnum.N: OrientationEnum.O,
+            OrientationEnum.O: OrientationEnum.S,
+            OrientationEnum.S: OrientationEnum.E,
+            OrientationEnum.E: OrientationEnum.N,
+        }
+        return mapping[self]
+
+    def turn_right(self):
+        mapping = {
+            OrientationEnum.N: OrientationEnum.E,
+            OrientationEnum.E: OrientationEnum.S,
+            OrientationEnum.S: OrientationEnum.O,
+            OrientationEnum.O: OrientationEnum.N,
+        }
+        return mapping[self]
+
+# Para definir choices en un campo de Django, creamos una lista de tuplas
+ORIENTATION_CHOICES = [(tag.value, tag.value) for tag in OrientationEnum]
